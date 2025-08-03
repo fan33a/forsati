b2d3cefe9ac6b20663b7b1e434dda6a0de6bf88f
Mohammed Muneifi <42098159+fan33a@users.noreply.github.com>
2025-08-03 18:31:09 +0300

    fix some problems

--- app/Http/Controllers/Api/FaqController.php
+++ app/Http/Controllers/Api/FaqController.php
@@ -4,46 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Faq;
 
 class FaqController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function getAllFaqs()
     {
-        //
-    }
+        $faqs = Faq::all();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'status' => 'success',
+            'data' => $faqs
+        ]);
     }
 }
